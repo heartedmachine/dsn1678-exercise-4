6934c63de5082e2ce8afaa5ea8390f03477d26f8
heartedmachine <r.melissa@icloud.com>
2014-10-09 10:05:59 -0400

    Making the shuttle move!

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 var $win = $(window);
 var $sun = $('.sun');
 var $sunSection = $('.sun-section');
-var $dipperSection = $('.dipper-section')
-var $dipper = $('.dipper')
+var $dipperSection = $('.dipper-section');
+var $dipper = $('.dipper');
+var $shuttleSection = $('.shuttle-section');
+var $shuttle = $('.shuttle');
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
@@ -13,4 +15,8 @@ $win.on('scroll', function () {
 
 $dipperSection.waypoint(function () {
 	$dipper.addClass('js-dipper-fade');
+}, { offset: '50%' });
+
+$shuttleSection.waypoint(function () {
+	$shuttle.addClass('js-shuttle-fade');
 }, { offset: '50%' });
